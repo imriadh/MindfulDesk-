1cae09355988dc1e73a7372035f3dc52db4471be
Riad Hossain <160244849+imriadh@users.noreply.github.com>
2026-02-12 17:24:51 +0000

    feat: add component barrel exports for better modularity

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Component barrel exports
+export { default as Dashboard } from './Dashboard';
+export { default as FocusTab } from './FocusTab';
+export { default as StatsTab } from './StatsTab';
+export { default as JournalTab } from './JournalTab';
+export { default as SettingsTab } from './SettingsTab';
